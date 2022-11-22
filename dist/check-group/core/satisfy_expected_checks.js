6201d9599205c95f0eca1818fb1b8cdbbbb5e0f2
Carlos Mocholí <carlossmocholi@gmail.com>
2022-11-22 01:21:24 +0100

    dist

--- dist/check-group/core/satisfy_expected_checks.js
+++ dist/check-group/core/satisfy_expected_checks.js
@@ -1,41 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.satisfyExpectedChecks = void 0;
-/* eslint-enable @typescript-eslint/no-unused-vars */
-/**
- * Checks if all the sub-project requirements are satisfied.
- *
- * @param subProjs The sub-projects a certain pull request
- * matches.
- *
- * @param checksStatusLookup The checks that has already
- * posted progresses. The key is the check ID and the value
- * is the current check status.
- *
- * @returns The current result of checks fulfillment.
- * * "all_passing" means all required checks post
- *   success conclusion.
- * * "has_failure" means at least one of the required
- *   checks failed.
- * * "pending" means there is no failure but some
- *   checks are pending or missing.
- */
+exports.satisfyExpectedChecks = exports.getCheckResult = void 0;
+var getCheckResult = function (checks, postedChecks) {
+    var result = "all_passing";
+    checks.forEach(function (check) {
+        var relevant = check in postedChecks;
+        if (relevant &&
+            postedChecks[check].conclusion !== "success" &&
+            postedChecks[check].conclusion !== null) {
+            // at least one check failed
+            return "has_failure";
+        }
+        if (!relevant || postedChecks[check].conclusion === null) {
+            // some checks are pending or missing
+            result = "pending";
+        }
+    });
+    return result;
+};
+exports.getCheckResult = getCheckResult;
 var satisfyExpectedChecks = function (subProjs, postedChecks) {
     var result = "all_passing";
-    subProjs.forEach(function (subProj) {
-        subProj.checks.forEach(function (check) {
-            if (check in postedChecks &&
-                postedChecks[check].conclusion !== "success" &&
-                postedChecks[check].conclusion !== null) {
-                result = "has_failure";
-            }
-            if ((!(check in postedChecks) ||
-                postedChecks[check].conclusion === null) &&
-                result !== "has_failure") {
-                result = "pending";
-            }
-        });
-    });
+    subProjs.forEach(function (subProj) { return result = (0, exports.getCheckResult)(subProj.checks, postedChecks); });
     return result;
 };
 exports.satisfyExpectedChecks = satisfyExpectedChecks;
